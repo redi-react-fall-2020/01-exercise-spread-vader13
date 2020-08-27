2e196b212ddf01781d9bf655b31675369e776a04
vader13 <hdedan.sy@gmail.com>
2020-08-27 17:31:01 +0200

    solved exercise 0, 1, 2, 3, 4, 5

--- student/index.js
+++ student/index.js
@@ -1,50 +1,50 @@
 // Exercise 0
 // Given a string,
 // return the string
-function returnTheString (string) {
-  // your code here
+function returnTheString(string) {
+  return string;
 }
 
 // Exercise 1
 // Given a string,
 // turn it into an array of the letters in the string
-function splitTheString (string) {
-  // your code here
+function splitTheString(string) {
+  return [...string];
 }
 
 // Exercise 2
 // Given an object,
 // return a shallow copy of the object
-function shallowCopyObject (object) {
-  // your code here
+function shallowCopyObject(object) {
+  return { ...object };
 }
 
 // Exercise 3
 // Given an array,
 // return a shallow copy of the array
-function shallowCopyArray (array) {
-  // your code here
+function shallowCopyArray(array) {
+  return [...array];
 }
 
 // Exercise 4
 // Given two objects,
 // turn them into one object that contains the properties of both objects
-function mergeTwoObjects (object1, object2) {
-  // your code here
+function mergeTwoObjects(object1, object2) {
+  return { ...object1, ...object2 };
 }
 
 // Exercise 5
 // Given two arrays,
 // turn them into one array containing all elements of both arrays
-function combineTwoArrays (array1, array2) {
-  // your code here
+function combineTwoArrays(array1, array2) {
+  return [...array1, ...array2];
 }
 
 module.exports = {
   returnTheString,
   splitTheString,
   shallowCopyObject,
-  shallowCopyArray,  
+  shallowCopyArray,
   mergeTwoObjects,
-  combineTwoArrays,
+  combineTwoArrays
 };
